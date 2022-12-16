ef3dec9ed222dfe0c2d9bd8183258f416daab2f3
Miriam Molina <miriam.molina.8323@gmail.com>
2022-12-15 19:21:33 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,13 @@ check.addEventListener('click', language);
 function language() {
   let id = check.checked;
   if (id == true) {
-    location.href = "/assets/es/index.html";
+    location.href = "https://miriammr.github.io/portfolio/assets/es/index.html";
     // location.href = "../es/index.html";
     document.getElementById('es').style.color = '#18d26e';
     document.getElementById("en").style.color = "#333";
     // location.href = "../assets/es/index.html";
   } else {
-    location.href = "/index.html";
+    location.href = "https://miriammr.github.io/portfolio/index.html";
     document.getElementById("es").style.color = "#333";
     document.getElementById("en").style.color = "#18d26e";
   }
